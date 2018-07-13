5972b8fed72c6ba1b6f8a304925310ffda9082cc
robsonklima <robsonklima@gmail.com>
2018-07-13 09:50:25 -0300

    Added post Delayed Loading in Login Activity and Styled it.

--- app/src/main/java/com/robsonlima/tickets/LoginActivity.java
+++ app/src/main/java/com/robsonlima/tickets/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.robsonlima.tickets;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,24 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onLogin(View view)
     {
-        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-        startActivity(intent);
+        progress = new ProgressDialog(this);
+        progress.setTitle("Authenticating");
+        progress.setMessage("Wait while authenticating...");
+        progress.setCancelable(false);
+        progress.show();
+
+        postDelayed();
+    }
+
+    private void postDelayed() {
+        new android.os.Handler().postDelayed(
+            new Runnable() {
+                public void run() {
+                    progress.dismiss();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }, 3000);
     }
 
     private void hideActionBar() {
